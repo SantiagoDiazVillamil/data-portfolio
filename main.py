c599d66efedf158d5c7383b2f38a748e343e4895
SantiagoDiazVillamil <diazvillamilsantiago@gmail.com>
2025-09-08 22:41:45 -0500

    Update main.py
    se adicionan nuevas páginas

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 from flask import Flask, render_template
 import os
 
-# Le decimos a Flask que busque plantillas en el mismo directorio
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+@app.route("/data-science")
+def data_science():
+    return render_template("data_science.html")
+
+@app.route("/churn")
+def churn():
+    return render_template("churn.html")
+
+@app.route("/anomalies")
+def anomalies():
+    return render_template("anomalies.html")
+
+@app.route("/nlp")
+def nlp():
+    return render_template("nlp.html")
+
+@app.route("/data-engineering")
+def data_engineering():
+    return render_template("data_engineering.html")
+
+@app.route("/pipelines")
+def pipelines():
+    return render_template("pipelines.html")
+
+@app.route("/streaming")
+def streaming():
+    return render_template("streaming.html")
+
+@app.route("/automation")
+def automation():
+    return render_template("automation.html")
