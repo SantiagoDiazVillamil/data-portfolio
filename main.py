93de7fb627794469ac6841fc088d131905d40ecf
SantiagoDiazVillamil <diazvillamilsantiago@gmail.com>
2025-09-08 22:03:46 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+# Le decimos a Flask que busque plantillas en el mismo directorio
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
